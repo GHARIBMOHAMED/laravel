d35d30d674d0120795bf8c2f13d4b6fe769b6796
gharib mohamed <gharibmalali@outlook.com>
2021-02-01 14:27:49 +0000

    style toaster

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,11 +38,23 @@ href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
 <script>
     @if(Session::has('message'))
-    toastr.options =
-    {
-        "closeButton" : true,
-        "progressBar" : true
-    }
+    toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+            };
             toastr.success("{{ session('message') }}");
     @endif
 
