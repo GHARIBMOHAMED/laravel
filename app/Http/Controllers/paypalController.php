6b465d0d97046453a2c76740f94e14413d0095fb
gharib mohamed <gharibmalali@outlook.com>
2021-02-22 10:44:34 +0000

    paypal

---
+++ app/Http/Controllers/paypalController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Paypal\Api\Amount;
+use Illuminate\Http\Request;
+
+class paypalController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
